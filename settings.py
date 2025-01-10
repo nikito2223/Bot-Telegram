1693fc4c2b820cb6b0b810ea266d9d5e034a8b5f
nikito2223 <107321123+nikito2223@users.noreply.github.com>
2025-01-10 19:24:09 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,6 +1,5 @@
 #SETTINGS
-BOT_TOKEN = "7588028456:AAEi13NrpNCgSHPyYaK9VBjP-z4WcVg-o48" #Токен бота
-#BOT_TOKEN = "7272257876:AAH7jOnrPR-Iv3xKln9vsM_qbLB5F3GQKhg"
+BOT_TOKEN = "<Toket-Your>" #Токен бота
 MAX_HEALTH = 99999 #Максимальное здоровия
 MAX_DAMAGE = 99999 #Максимальный урон
 
